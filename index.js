0b1238685df79c8d3a5b0cdf0b67a892fc7f7ae3
Daniel Di Venere <danieldivenere@gmail.com>
2019-10-26 11:37:46 -0700

    added ability to add to list

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-function main() {
+function submitInput() {
     $('#js-shopping-list-form').submit(function (event) {
         event.preventDefault();
         const input = $('#shopping-list-entry').val();
         $('#shopping-list-entry').val("");
+        $('.shopping-list').append(`<li><span class='shopping-item'>${input}</span><div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>check</span></button> <button class='shopping-item-delete'><span class='button-label'>delete</span></button></div></li>`);
     });
 }
 
-$(main);
+$(() => {
+    $(submitInput);
+});
