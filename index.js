a09e58e755d693f3e1b9f86f600e192fcd907759
Daniel Di Venere <danieldivenere@gmail.com>
2019-10-26 13:16:25 -0700

    attempted to restructure with array

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+const STORE = [
+    {name: "apples", checked: false}, 
+    {name: "oranges", checked: false},
+    {name: "milk", checked: true},
+    {name: "bread", checked: false},
+];
+
+function renderList() {
+    $('.shopping-list').find('li').remove();
+    for(let i = 0; i < STORE.length; i++) {
+        $('.shopping-list').append(`<li>
+        <span class="shopping-item ${STORE[i].checked ? "shopping-item__checked" : ""}">${STORE[i].name}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+    }
+}
+
 function submitInput() {
     $('#js-shopping-list-form').submit(function (event) {
         event.preventDefault();
-        const input = $('#shopping-list-entry').val();
+        STORE.push({name: $('#shopping-list-entry').val(), checked: false});
         $('#shopping-list-entry').val("");
-        $('.shopping-list').append(`<li><span class='shopping-item'>${input}</span><div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>check</span></button> <button class='shopping-item-delete'><span class='button-label'>delete</span></button></div></li>`);
+        renderList();
     });
 }
 
+// need to figure out how to remove and check entries using STORE array
 function deleteEntry() {
     $('.shopping-list').on('click', '.shopping-item-delete', function (event) {
         this.closest('li').remove();
+        // console.log(event);
     });
 }
 
@@ -20,6 +46,7 @@ function checkEntry() {
 }
 
 function main() {
+    renderList();
     submitInput();
     deleteEntry();
     checkEntry();
