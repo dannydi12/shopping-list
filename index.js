cb79fc22ee83e4ce0b234a68e94d872ef1d097b5
Daniel Di Venere <danieldivenere@gmail.com>
2019-10-26 12:36:59 -0700

    restructured

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ function checkEntry() {
     });
 }
 
-$(() => {
-    $(submitInput);
-    $(deleteEntry);
-    $(checkEntry);
-});
+function main() {
+    submitInput();
+    deleteEntry();
+    checkEntry();
+}
+
+$(main);
